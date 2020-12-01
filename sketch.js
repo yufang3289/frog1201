742ecc9658a77e6fe241b520df066b15ed2fcc35
yufang3289 <71304479+yufang3289@users.noreply.github.com>
2020-12-01 11:46:37 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://yufang3289.github.io/frog1201/model.json';
 
 
 function preload() {
